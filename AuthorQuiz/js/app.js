e167690f0661f8705258c8a2d111d01965537346
Aaron Muylaert <aaron@tactics.be>
2014-08-29 11:44:37 +0200

    Initial commit.

---
+++ AuthorQuiz/js/app.js
@@ -0,0 +1,23 @@
+/** @jsx React.DOM */
+
+(function () {
+    'use strict';
+
+    var Quiz = React.createClass({
+        render: function () {
+            return <div>
+                {this.props.books.map(function(book) {
+                    return <Book title={book} />;
+                })}
+            </div>
+        }
+    });
+
+    var Book = React.createClass({
+        render: function () {
+            return <div><h4>{this.props.title}</h4></div>;
+        }
+    });
+
+    React.renderComponent(<Quiz books={['The Lord of The Rings', 'The Iliad']}/>, document.getElementById('app'));
+})();
